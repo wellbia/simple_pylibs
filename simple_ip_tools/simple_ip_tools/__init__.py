8bd08b53cb02f63fdf4fdb9c54551aa499bb1d7a
hgyoon <hgyoon@wellbia.com>
2025-04-18 15:09:28 +0900

    simple_ip_tools:: Remove registered_country in network to country

--- simple_ip_tools/simple_ip_tools/__init__.py
+++ simple_ip_tools/simple_ip_tools/__init__.py
@@ -240,8 +240,5 @@ def __load_network_to_registered_country(csv_path):
         for row in reader:
             network = row["network"].strip()
             reg_id = row["geoname_id"].strip()
-            if reg_id.empty():
-                reg_id = row["registered_country_geoname_id"].strip()
-
             mapping[network] = int(reg_id) if reg_id else None
     return mapping
